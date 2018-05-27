477a6d7397e0a386d3d2a2ae31fff6f61524c3ea
HomeKiran <homekiran@gmail.com>
2018-05-27 10:16:32 +0530

    Home:Python Updated

--- PythonPrograms/RuffScript.py
+++ PythonPrograms/RuffScript.py
@@ -1,4 +1,9 @@
-import builtins
-a=10
-print(a)
-print(dir(builtins))
+# import urllib2
+#
+# def internet_on():
+#     try:
+#         urllib2.urlopen('http://216.58.192.142', timeout=1)
+#         return True
+#     except urllib2.URLError as err:
+#         return False
+
