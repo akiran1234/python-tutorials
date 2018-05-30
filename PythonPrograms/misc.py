b4399744d745690487afb397c041634d724fce98
OfficeKiran <officekiran@gmail.com>
2018-05-30 12:48:30 +0530

    Office:Conflicts Fixed

---
+++ PythonPrograms/misc.py
@@ -0,0 +1,5 @@
+import math
+
+print(format(12, "08b")) # creating whole number to binary, 08b leading zero and total length 8.
+print(int('1111',2))     # creating binary number to whole number.
+print(abs.__doc__)       # doc string used against a fuction to get the info.
