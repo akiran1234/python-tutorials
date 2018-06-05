635c615ef7024f874adf72e20e5978587ad63de5
OfficeKiran <officekiran@gmail.com>
2018-06-05 13:18:06 +0530

    office:Updated Python

---
+++ PythonPrograms/OOPS/Class&Object&Constructor.py
@@ -0,0 +1,44 @@
+# A class is a blue print or a template which is logical in memory.
+# An object is instance of the class which is actual and occupies memory.
+# A class is defined with key word class and object is created by calling the class name with parenthesis.
+# In python object it self is passed as an argument to the class hence self is used as the first argument
+# .. in constructor and all the methods of the class.
+
+class student:           # Creating class student with class keyword.
+    def hello(self):     # Defining a method inside class
+        print("I'm in student class")
+
+s1 = student()           # Creating an object by simply calling the class the same as function.
+s1.hello()               # Calling the method with instance s1 and s1 itself is passed as an argument.
+print(type(s1))          # Checking the type of object s1
+print(type(s1.hello))    # Checking the type of method
+
+
+# Constructor in python
+# constructor is a special method in python with name __init(self) and gets executed when ever instance is created.
+# It is used to initialize the instance variables and this variables are visible to all the methods of a class.
+# constructor vs method
+# 1. Constructor will start with name __init__				 1. method can have any name
+# 2. It is used to initialize the instance variables.        2. It is used to write business logic inside.
+# 3. It is executed implicitly when ever object is created.  3. It is executed explicitly from an object.
+
+class college:
+
+    def __init__(self):
+        print("I am a constructor,I will be executed for every instance created")
+
+c1=college()          # constructor invoked implicitly for object creation c1 with out args.
+c2=college()          # constructor invoked implicitly for object creation c2
+
+# Invoking constructor with arguments if args not match it will throw error.
+
+class college:
+
+    def __init__(self,a,b):
+        self.z=a+b
+        print("Printing sum of Instance Variables",self.z)
+
+c1=college(10,20)    # While creating the instance itself constructor will be executed with this arguments.
+
+
+
