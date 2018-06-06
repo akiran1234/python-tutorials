2171a971d250112fc869f5bc7efd4916537962a7
OfficeKiran <officekiran@gmail.com>
2018-06-06 18:13:29 +0530

    Office:Update Python

--- PythonPrograms/RuffScript.py
+++ PythonPrograms/RuffScript.py
@@ -1,12 +1,21 @@
-from datetime import date
-from datetime import time
-from datetime import datetime
 import numpy as np
 import numpy as np
 
 np.random.random_sample()
 
 np.random.randint()
+
+a=np.array([40,20,10,40,15,23.5,True])
+
+print(a[:len(a)-1][0:2])
+
+
+
+
+
+
+
+
 
 
 
