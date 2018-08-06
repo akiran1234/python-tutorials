0bccb2795f1c275d14eae525d99c38ae2cf670fb
HomeKiran <homekiran@gmail.com>
2018-08-06 21:38:43 +0530

    Home:Python Pandas Updated

--- PythonPrograms/RuffScript.py
+++ PythonPrograms/RuffScript.py
@@ -1,19 +1,8 @@
-import numpy as np
-import pandas as pd
+l=[1,2,3,4,5,6,7,8,9,10]
 
 
 
-a=np.arange(25).reshape(5,5)
+x=[i for i in l if i%2==0]
+print(x)
 
-print(type(a),a,sep='\n')
-print(a[a>5])
-
-
-
-
-
-
-
-
-
-
+print('Hi this is kiran')
