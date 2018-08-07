f1ceab56937f56bab246f071f01d7e60130e07de
OfficeKiran <officekiran@gmail.com>
2018-08-07 11:47:49 +0530

    Python updated

--- PythonPrograms/RuffScript.py
+++ PythonPrograms/RuffScript.py
@@ -1,27 +1,7 @@
-class test:
-    def __init__(self):
-        print("Hello I'm in constructor")
-
-    @staticmethod
-    def hellostatic():
-        print("Hey this is static method, No need of Instance creation")
-
-    @classmethod
-    def greet(cls):
-        print("Hey this is class method")
-
-
-test.hellostatic()
-
-l=list(range(100))
-print([x for x in l if x%2==0])
-
-
-
-
-
-
-
-
-
+a=['hello','ram','ramesh','david']
+# print(type(a))
+# print(a.sort(),a)
+print(dir(a))
+i=iter(a)
+print(next(i))
 
